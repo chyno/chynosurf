0714115c8591dcb3412087d3e3a25b8f3afca24a
jxc14 <John.Chynoweth-contractor@nreca.coop>
2015-02-11 14:25:45 -0500

    wind router works

--- dist/wind.js
+++ dist/wind.js
@@ -1,53 +1,20 @@
-System.register(["aurelia-http-client"], function (_export) {
+System.register([], function (_export) {
   "use strict";
 
-  var HttpClient, _prototypeProperties, _classCallCheck, url, Wind;
+  var _classCallCheck, url, Wind;
   return {
-    setters: [function (_aureliaHttpClient) {
-      HttpClient = _aureliaHttpClient.HttpClient;
-    }],
+    setters: [],
     execute: function () {
-      _prototypeProperties = function (child, staticProps, instanceProps) { if (staticProps) Object.defineProperties(child, staticProps); if (instanceProps) Object.defineProperties(child.prototype, instanceProps); };
-
       _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
 
       url = "http://tidesandcurrents.noaa.gov/api/datagetter?begin_date=20130808%2015:00&end_date=20130808%2015:06&station=8454000&product=wind&units=english&time_zone=gmt&application=ports_screen&format=json";
-      Wind = _export("Wind", (function () {
-        function Wind(http) {
-          _classCallCheck(this, Wind);
+      Wind = _export("Wind", function Wind() {
+        _classCallCheck(this, Wind);
 
-          this.heading = "Wind Information";
-          this.readings = [];
-          this.http = http;
-        }
-
-        _prototypeProperties(Wind, {
-          inject: {
-            value: function inject() {
-              return [HttpClient];
-            },
-            writable: true,
-            configurable: true
-          }
-        }, {
-          activate: {
-            value: function activate() {
-              var _this = this;
-              return this.http.jsonp(url).then(function (response) {
-                var data = response.content;
-                if (data) {
-                  _this.readings = [];
-                }
-              });
-            },
-            writable: true,
-            configurable: true
-          }
-        });
-
-        return Wind;
-      })());
+        this.heading = "Wind Information";
+        this.readings = [];
+      });
     }
   };
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndpbmQuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O01BQVEsVUFBVSx5Q0FLZCxHQUFHLEVBRU0sSUFBSTs7O0FBUFQsZ0JBQVUsc0JBQVYsVUFBVTs7Ozs7OztBQUtkLFNBQUcsR0FBRyxxTUFBcU07QUFFbE0sVUFBSTtBQUVKLGlCQUZBLElBQUksQ0FFSCxJQUFJO2dDQUZMLElBQUk7O0FBR2IsY0FBSSxDQUFDLE9BQU8sR0FBRyxrQkFBa0IsQ0FBQztBQUNsQyxjQUFJLENBQUMsUUFBUSxHQUFHLEVBQUUsQ0FBQztBQUNuQixjQUFJLENBQUMsSUFBSSxHQUFHLElBQUksQ0FBQztTQUNsQjs7NkJBTlUsSUFBSTtBQUNSLGdCQUFNO21CQUFBLGtCQUFHO0FBQUUscUJBQU8sQ0FBQyxVQUFVLENBQUMsQ0FBQzthQUFFOzs7OztBQU94QyxrQkFBUTttQkFBQSxvQkFBRTs7QUFDUixxQkFBTyxJQUFJLENBQUMsSUFBSSxDQUFDLEtBQUssQ0FBQyxHQUFHLENBQUMsQ0FBQyxJQUFJLENBQUMsVUFBQSxRQUFRLEVBQUk7QUFDM0Msb0JBQUksSUFBSSxHQUFHLFFBQVEsQ0FBQyxPQUFPLENBQUM7QUFDNUIsb0JBQUksSUFBSSxFQUNSO0FBQ0Usd0JBQUssUUFBUSxHQUFHLEVBQUUsQ0FBQztpQkFDcEI7ZUFFRixDQUFDLENBQUM7YUFDSjs7Ozs7O2VBakJVLElBQUkiLCJmaWxlIjoid2luZC5qcyIsInNvdXJjZVJvb3QiOiIvc3JjLyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndpbmQuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O3VCQUtJLEdBQUcsRUFFTSxJQUFJOzs7Ozs7QUFGYixTQUFHLEdBQUcscU1BQXFNO0FBRWxNLFVBQUksbUJBRUosU0FGQSxJQUFJOzhCQUFKLElBQUk7O0FBR2IsWUFBSSxDQUFDLE9BQU8sR0FBRyxrQkFBa0IsQ0FBQztBQUNsQyxZQUFJLENBQUMsUUFBUSxHQUFHLEVBQUUsQ0FBQztPQUVwQiIsImZpbGUiOiJ3aW5kLmpzIiwic291cmNlUm9vdCI6Ii9zcmMvIn0=
--- src/wind.js
+++ src/wind.js
@@ -1,23 +1,17 @@
-import {HttpClient} from 'aurelia-http-client';
+ 
 /*
 {"metadata":{"id":"8454000","name":"Providence","lat":"41.8071","lon":"-71.4012"}, 
 "data": [{"t":"2013-08-08 15:00", "s":"10.89", "d":"236.00", "dr":"SW", "g":"15.94", "f":"0,0"},{"t":"2013-08-08 15:06", "s":"6.80", "d":"222.00", "dr":"SW", "g":"11.86", "f":"0,0"}]}
 */
 var url = 'http://tidesandcurrents.noaa.gov/api/datagetter?begin_date=20130808%2015:00&end_date=20130808%2015:06&station=8454000&product=wind&units=english&time_zone=gmt&application=ports_screen&format=json';
 
-export class Wind{
- 
-  }
- 
+export class Wind {
+  
+  constructor(){
+    this.heading = 'Wind Information';
+    this.readings = [];
    
-/*
-  class WindSetting {
-  constructor(speed, gusts, direction)
-  {
-    this.speed = speed;
-    this.gusts = gusts;
-    this.direction = direction;
   }
 }
-*/
-}
+ 
+
