7a970af99da2cb8741952795715fd0967777dc0c
jxc14 <John.Chynoweth-contractor@nreca.coop>
2015-02-11 14:06:31 -0500

    clean up

--- src/wind.js
+++ src/wind.js
@@ -6,22 +6,7 @@ import {HttpClient} from 'aurelia-http-client';
 var url = 'http://tidesandcurrents.noaa.gov/api/datagetter?begin_date=20130808%2015:00&end_date=20130808%2015:06&station=8454000&product=wind&units=english&time_zone=gmt&application=ports_screen&format=json';
 
 export class Wind{
-  static inject() { return [HttpClient]; }
-  constructor(http){
-    this.heading = 'Wind Information';
-    this.readings = [];
-    this.http = http;
-  }
-
-  activate(){
-    return this.http.jsonp(url).then(response => {
-      var data = response.content.data;
-      if (data)
-      {
-        this.readings = []; // data.map(new WindSetting(d.s,d.g,dr.sw));
-      }
-      
-    });
+ 
   }
  
    
