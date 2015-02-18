a9b0113f675dc8e8628c1189f62eb3509543e1e5
jxc14 <John.Chynoweth-contractor@nreca.coop>
2015-02-18 15:58:44 -0500

    test files

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -1,23 +1,21 @@
-/*
+var gulp = require('gulp');
 var karma = require('gulp-karma');
  
-var testFiles = [
-  'client/todo.js',
-  'client/todo.util.js',
-  'client/todo.App.js',
-  'test/client/*.js'
-];
+
+ 
+var testFiles = [];
  
 gulp.task('test', function() {
   // Be sure to return the stream 
   return gulp.src(testFiles)
     .pipe(karma({
       configFile: 'karma.conf.js',
-      action: 'run'
+      action: 'start'
     }))
     .on('error', function(err) {
       // Make sure failed tests cause gulp to exit non-zero 
+      console.log('.. Error running test');
+      console.log(err);
       throw err;
     });
 });
-*/
