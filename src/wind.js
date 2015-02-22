ef34892fc7f74dac5304c6161c645a99f3eafe7e
John Chynoweth <jchynoweth@vmdsystems.com>
2015-02-22 00:03:18 -0500

    fix up code

--- src/wind.js
+++ src/wind.js
@@ -2,8 +2,7 @@
 {"metadata":{"id":"8454000","name":"Providence","lat":"41.8071","lon":"-71.4012"}, 
 "data": [{"t":"2013-08-08 15:00", "s":"10.89", "d":"236.00", "dr":"SW", "g":"15.94", "f":"0,0"},{"t":"2013-08-08 15:06", "s":"6.80", "d":"222.00", "dr":"SW", "g":"11.86", "f":"0,0"}]}
 */
-//var url = 'http://tidesandcurrents.noaa.gov/api/datagetter?begin_date=20130808%2015:00&end_date=20130808%2015:06&station=8454000&product=wind&units=english&time_zone=gmt&application=ports_screen&format=json';
-
+var url = 'http://tidesandcurrents.noaa.gov/api/datagetter?begin_date=20130808%2015:00&end_date=20130808%2015:06&station=8454000&product=wind&units=english&time_zone=gmt&application=ports_screen&format=json';
 import {WindService} from './services/wind-service';
 
  export class Wind{
@@ -16,10 +15,15 @@ import {WindService} from './services/wind-service';
   }
 
   activate(){
-     
-  this.service.readings().then(data => {
+     /*
+   return this.service.readings().then(data => {
       this.readings = data;
     });
+*/
+    return $http(url).get().then(response =>
+      {
+             console.log('response :' + response);
+      });
   }  
 }
 
